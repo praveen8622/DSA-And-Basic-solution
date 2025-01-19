464d4d891a87f1c9ca26b94e1a01c2b8bd1647d6
prvn <karnapraveen22@gmail.com>
2025-01-19 12:58:24 +0545

    complete question

---
+++ practice.py
@@ -0,0 +1,9 @@
+# finding largest element from array
+arr = [1, 2, 3, 4, 5, 6]
+largest = arr[0]
+for i in arr:
+    if i > largest:
+        largest = i
+print(largest)
+
+# finding second largest element from array
