9a5754ad30697553d8ad79b7036fe8e860e7faae
prvn <karnapraveen22@gmail.com>
2025-01-19 17:31:29 +0545

    finding largest number

--- practice.py
+++ practice.py
@@ -6,4 +6,33 @@ for i in arr:
         largest = i
 print(largest)
 
+# another method 
+def largest (arr, n ):
+    max = arr[0]
+
+    for i in range (1,n):
+        if arr[i]> max:
+            max = arr[i]
+    return max
+arr = [1, 2, 3, 4, 5, 6]
+n= len(arr)
+answer = largest(arr, n)
+print ("Largest number is :" ,answer)
+
+# sorting an array in ascending
+# price = [10, 12, 16, 9, 8, 3, 20, 45, 3, 7]
+# price.sort(reverse=False)
+# print(price)
+
 # finding second largest element from array
+
+price = [10, 12, 16, 9, 8, 3, 20, 45, 3, 7]
+
+
+def myFunc(reverse: False):
+    return reverse
+
+for i in price: 
+    if 
+price.sort(key=myFunc)
+print(price)
