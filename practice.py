309c9f76f4017652e55ce1a846aa56ae468bfbac
prvn <karnapraveen22@gmail.com>
2025-01-19 21:56:53 +0545

    finding 2nd largest number

--- practice.py
+++ practice.py
@@ -6,18 +6,21 @@ for i in arr:
         largest = i
 print(largest)
 
-# another method 
-def largest (arr, n ):
+
+# another method
+def largest(arr, n):
     max = arr[0]
 
-    for i in range (1,n):
-        if arr[i]> max:
+    for i in range(1, n):
+        if arr[i] > max:
             max = arr[i]
     return max
+
+
 arr = [1, 2, 3, 4, 5, 6]
-n= len(arr)
+n = len(arr)
 answer = largest(arr, n)
-print ("Largest number is :" ,answer)
+print("Largest number is :", answer)
 
 # sorting an array in ascending
 # price = [10, 12, 16, 9, 8, 3, 20, 45, 3, 7]
@@ -26,13 +29,19 @@ print ("Largest number is :" ,answer)
 
 # finding second largest element from array
 
-price = [10, 12, 16, 9, 8, 3, 20, 45, 3, 7]
+price = [10, 12, 16, 9, 8, 3, 20, 45, 45, 3, 7]
 
 
-def myFunc(reverse: False):
-    return reverse
+def secLargest(price, p):
+    price.sort(reverse=True)
+    print(price)
+    for a in range(1, n):
+        if price[a] != price[0]:
+            sec_larg = price[a]
+            break
+    return sec_larg
 
-for i in price: 
-    if 
-price.sort(key=myFunc)
-print(price)
+
+p = len(price)
+ans = secLargest(price, p)
+print(ans)
